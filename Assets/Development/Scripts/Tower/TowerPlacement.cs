1cf66fd208f937cac3b967ca2a640897dad37a00
Rink Wilbrink <rinkwilbrink1@gmail.com>
2020-10-07 15:25:18 +0200

    Added TMP and started with Placing towers

---
+++ Assets/Development/Scripts/Tower/TowerPlacement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private GameObject[] TowerList;
+    
+    public void OnButtonClick(int _i)
+    {
+
+    }
+}

00ea70abb949b60f491e7d34c4d19c86f8abc238
Rink Wilbrink <rinkwilbrink1@gmail.com>
2020-10-30 14:18:41 +0100

    3rd build version

--- Assets/Development/Scripts/Enemy AI/EnemyUnit.cs
+++ Assets/Development/Scripts/Enemy AI/EnemyUnit.cs
@@ -10,7 +10,7 @@ public class EnemyUnit : MonoBehaviour
 
     public int Health { get; private set; }
     public float Speed { get; private set; }
-    public float GoldReward { get; private set; }
+    public int GoldReward { get; private set; }
     public int AttackDamage { get; private set; }
 
     public event Action OnDeath;
@@ -18,21 +18,24 @@ public class EnemyUnit : MonoBehaviour
     public Transform[] wayPoints;
     private int waypointIndex;
 
-    public void Initialize( EnemyData e ) {
+    public void Initialize(EnemyData e)
+    {
         this.Health = e.health;
         this.Speed = e.speed;
         this.GoldReward = e.goldReward;
         this.AttackDamage = e.attackDamage;
     }
 
-    private void Start() {
+    private void Start()
+    {
         //wayPoints = FindObjectOfType<WaypointManager>();
 
         // The values can be decided here but we need to figure out what type of enemy unit we are first
         Initialize(enemyData);
     }
 
-    private void Update() {
+    private void Update()
+    {
         transform.position = Vector3.MoveTowards(transform.position, wayPoints[waypointIndex].position, Speed * Time.deltaTime);
 
         // Need to test the rotation more
@@ -42,16 +45,18 @@ public class EnemyUnit : MonoBehaviour
         //Vector3 dir = WayPointManager.waypoints[waypointIndex].position - transform.position;
         //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Mathf.Atan2(dir.x, dir.y) / Mathf.PI * 180, 0), 0.1f);
 
-        if ( Input.GetKeyDown(KeyCode.E) )
+        if(Input.GetKeyDown(KeyCode.E))
             TakeDamage(1);
 
-        if ( Speed < 0 )
+        if(Speed < 0)
             Speed = 0;
 
-        if ( slowDebuffActive ) {
+        if(slowDebuffActive)
+        {
             slowDebuffTimer += Time.deltaTime;
 
-            if ( slowDebuffTimer > slowDebuffTime ) {
+            if(slowDebuffTimer > slowDebuffTime)
+            {
                 Speed += slowDownSpeed;
                 slowDebuffTimer = 0f;
                 slowDebuffActive = false;
@@ -59,48 +64,46 @@ public class EnemyUnit : MonoBehaviour
         }
 
         // Test
-        if ( Input.GetKeyDown(KeyCode.S) )
+        if(Input.GetKeyDown(KeyCode.S))
             SlowDown(80f, 4f);
 
-        if ( Vector3.Distance(transform.position, wayPoints[waypointIndex].position) < .1f )
-            if ( waypointIndex < wayPoints.Length - 1 ) {
+        if(Vector3.Distance(transform.position, wayPoints[waypointIndex].position) < .1f)
+            if(waypointIndex < wayPoints.Length - 1)
+            {
                 waypointIndex++;
-            } else {
+            }
+            else
+            {
                 Death();
                 GameController.MainTowerHP -= AttackDamage;
                 // Do damage to the main structure
             }
     }
 
-    public void TakeDamage( int damage ) {
+    public void TakeDamage(int damage)
+    {
         Health -= damage;
 
-        if ( Health < 1 )
+        if(Health < 1)
+        {
             Death();
+            GameController.Gold += GoldReward;
+        }
     }
-
-    //bool takeDamageOTActive = false;
-    //int takeDamageOT;
-    //float takeDamageOTTime;
-    //float takeDamageOTTimer = 0;
-    //public void TakeDamageOverTime( int totalDamage, float time ) {
-    //    takeDamageOT = totalDamage;
-    //    takeDamageOTTime = time;
-
-    //    takeDamageOTActive = true;
-    //}
 
     bool slowDebuffActive = false;
     float slowDownSpeed;
     float slowDownTotalSpeed;
     float slowDebuffTime;
     float slowDebuffTimer = 0;
-    public void SlowDown( float speedDebuff, float time ) {
-        if ( Speed > slowDownTotalSpeed ) {
-            slowDownSpeed = ( speedDebuff / 100 ) * Speed;
+    public void SlowDown(float speedDebuff, float time)
+    {
+        if(Speed > slowDownTotalSpeed)
+        {
+            slowDownSpeed = (speedDebuff / 100) * Speed;
 
-            print(( speedDebuff / 100 ) * Speed);
-            Speed -= ( speedDebuff / 100 ) * Speed;
+            print((speedDebuff / 100) * Speed);
+            Speed -= (speedDebuff / 100) * Speed;
 
             slowDownTotalSpeed = Speed;
             slowDebuffTime = time;
@@ -109,10 +112,11 @@ public class EnemyUnit : MonoBehaviour
         }
     }
 
-    private void Death() {
+    private void Death()
+    {
         Destroy(gameObject);
 
-        if ( OnDeath != null )
+        if(OnDeath != null)
             OnDeath();
     }
 }

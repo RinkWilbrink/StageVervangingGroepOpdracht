d37d0bfac9a7986d23802f07d9c490340cb9ddd2
Rink Wilbrink <38427944+RinkWilbrink@users.noreply.github.com>
2020-11-03 14:46:39 +0100

    added a new function that checks if the enemy already has poison effect in it

--- Assets/Development/Scripts/Enemy AI/EnemyUnit.cs
+++ Assets/Development/Scripts/Enemy AI/EnemyUnit.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyUnit : MonoBehaviour
@@ -18,77 +19,87 @@ public class EnemyUnit : MonoBehaviour
     public Transform[] wayPoints;
     private int waypointIndex;
 
-    public void Initialize( EnemyData e ) {
+    public void Initialize(EnemyData e)
+    {
         this.Health = e.health;
         this.Speed = e.speed;
         this.GoldReward = e.goldReward;
         this.AttackDamage = e.attackDamage;
     }
 
-    private void Start() {
-        //wayPoints = FindObjectOfType<WaypointManager>();
-
+    private void Start()
+    {
         // The values can be decided here but we need to figure out what type of enemy unit we are first
         Initialize(enemyData);
     }
 
-    private void Update() {
+    private void Update()
+    {
         transform.position = Vector3.MoveTowards(transform.position, wayPoints[waypointIndex].position, Speed * Time.deltaTime);
 
         // Need to test the rotation more
         Quaternion dir = Quaternion.LookRotation(wayPoints[waypointIndex].position - transform.position);
         transform.rotation = Quaternion.Lerp(transform.rotation, dir, rotateSpeed * Time.deltaTime);
 
-        //Vector3 dir = WayPointManager.waypoints[waypointIndex].position - transform.position;
-        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, Mathf.Atan2(dir.x, dir.y) / Mathf.PI * 180, 0), 0.1f);
-
-        if ( Input.GetKeyDown(KeyCode.E) )
-            TakeDamage(1);
-
-        if ( Speed < 0 )
+        if (Speed < 0)
+        {
             Speed = 0;
+        }
 
-        if ( slowDebuffActive ) {
+        if (slowDebuffActive)
+        {
             slowDebuffTimer += Time.deltaTime;
 
-            if ( slowDebuffTimer > slowDebuffTime ) {
+            if (slowDebuffTimer > slowDebuffTime)
+            {
                 Speed += slowDownSpeed;
                 slowDebuffTimer = 0f;
                 slowDebuffActive = false;
             }
         }
-
-        // Test
-        if ( Input.GetKeyDown(KeyCode.S) )
-            SlowDown(80f, 4f);
-
-        if ( Input.GetKeyDown(KeyCode.G) )
-            StartCoroutine(TakeDamageOverTime(1, 2));
-
-        if ( Vector3.Distance(transform.position, wayPoints[waypointIndex].position) < .1f )
-            if ( waypointIndex < wayPoints.Length - 1 ) {
+        if (Vector3.Distance(transform.position, wayPoints[waypointIndex].position) < .1f)
+        {
+            if (waypointIndex < wayPoints.Length - 1)
+            {
                 waypointIndex++;
-            } else {
+            }
+            else
+            {
                 Death();
                 GameController.MainTowerHP -= AttackDamage;
                 // Do damage to the main structure
             }
+        }
     }
 
-    public void TakeDamage( int damage ) {
+    public void TakeDamage(int damage)
+    {
         Health -= damage;
 
-        if ( Health < 1 )
+        if (Health < 1)
             Death();
     }
 
     [NonSerialized] public int takeDamageOTTimer = 0;
     private bool takeDamageOTActive = false;
-    public IEnumerator TakeDamageOverTime( int dps, int damageTime, int timeUntilDamageTaken = 1 ) {
-        takeDamageOTActive = true;
-        takeDamageOTTimer = 0;
 
-        while ( takeDamageOTTimer < damageTime ) {
+    public void PoisonDOT(int dps, int damageTime, int timeUntilDamageTaken = 1)
+    {
+        if(takeDamageOTActive == false)
+        {
+            takeDamageOTActive = true;
+            takeDamageOTTimer = 0;
+            StartCoroutine(TakeDamageOverTime(dps, damageTime, timeUntilDamageTaken));
+        }
+        else
+        {
+            takeDamageOTTimer = 0;
+        }
+    }
+    private IEnumerator TakeDamageOverTime(int dps, int damageTime, int timeUntilDamageTaken = 1)
+    {
+        while (takeDamageOTTimer < damageTime)
+        {
             Health -= dps;
             yield return new WaitForSeconds(timeUntilDamageTaken);
             takeDamageOTTimer++;
@@ -96,8 +107,10 @@ public class EnemyUnit : MonoBehaviour
 
         takeDamageOTActive = false;
 
-        if ( Health < 1 )
+        if (Health < 1)
+        {
             Death();
+        }
     }
 
     bool slowDebuffActive = false;
@@ -105,12 +118,14 @@ public class EnemyUnit : MonoBehaviour
     float slowDownTotalSpeed;
     float slowDebuffTime;
     float slowDebuffTimer = 0;
-    public void SlowDown( float speedDebuff, float time ) {
-        if ( Speed > slowDownTotalSpeed ) {
-            slowDownSpeed = ( speedDebuff / 100 ) * Speed;
+    public void SlowDown(float speedDebuff, float time)
+    {
+        if (Speed > slowDownTotalSpeed)
+        {
+            slowDownSpeed = (speedDebuff / 100) * Speed;
 
-            print(( speedDebuff / 100 ) * Speed);
-            Speed -= ( speedDebuff / 100 ) * Speed;
+            print((speedDebuff / 100) * Speed);
+            Speed -= (speedDebuff / 100) * Speed;
 
             slowDownTotalSpeed = Speed;
             slowDebuffTime = time;
@@ -119,10 +134,11 @@ public class EnemyUnit : MonoBehaviour
         }
     }
 
-    private void Death() {
+    private void Death()
+    {
         Destroy(gameObject);
 
-        if ( OnDeath != null )
+        if (OnDeath != null)
             OnDeath();
     }
 }

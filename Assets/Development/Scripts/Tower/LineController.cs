793c0df379cb179fa11bad2b0c097561408a8971
Emerson Brinks <emerson_10@hotmail.nl>
2021-01-21 12:59:35 +0100

    added animations for enemies and building animations for towers

---
+++ Assets/Development/Scripts/Tower/LineController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+
+    private LineRenderer lineRenderer;
+    private Transform target1;
+    private Transform target2;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, target1.transform.position);
+        lineRenderer.SetPosition(1, target2.transform.position);
+    }
+
+    public void AssignTarget(Transform startPosition, Transform endPosition)
+    {
+        lineRenderer.positionCount = 2;
+
+        target1 = startPosition;
+        target2 = endPosition;
+    }
+}
